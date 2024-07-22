97d15248d5b9246ac3bde9c8f5db2ab688d24cb8
Elon Aseneka <elonsantos63@gmail.com>
2024-07-22 21:38:07 +0300

    test webhook

--- services/payments/src/rest/handlers.rs
+++ services/payments/src/rest/handlers.rs
@@ -7,4 +7,10 @@ use lib::utils::models::UserPaymentDetails;
 use std::{fs::File, io::Write, sync::Arc};
 use surrealdb::{engine::remote::ws::Client, Surreal};
 
-pub async fn handle_paystack_webhook(Extension(db): Extension<Arc<Surreal<Client>>>, mut body: Json<UserPaymentDetails>) {}
+use crate::graphql::schemas::paystack::ChargeEvent;
+
+pub async fn handle_paystack_webhook(Extension(db): Extension<Arc<Surreal<Client>>>, mut body: Json<ChargeEvent>) {
+    if body.event == "charge.success".to_string() {
+        println!("body: {:?}", body);
+    }
+}
