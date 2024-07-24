2666ff385dce5ddf061def3b1ff4c55a7434148d
Elon Aseneka <elonsantos63@gmail.com>
2024-07-24 21:06:51 +0300

    try fix env vars errors in REST routes

--- services/payments/src/main.rs
+++ services/payments/src/main.rs
@@ -24,6 +24,7 @@ use rest::handlers::handle_paystack_webhook;
 // use serde::Deserialize;
 use surrealdb::{engine::remote::ws::Client, Result, Surreal};
 use tower_http::cors::CorsLayer;
+use dotenvy::dotenv;
 
 use graphql::resolvers::mutation::Mutation;
 
@@ -43,6 +44,7 @@ async fn graphql_handler(
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
     let db = Arc::new(database::connection::create_db_connection().await.unwrap());
 
     let schema = Schema::build(Query::default(), Mutation::default(), EmptySubscription).finish();
