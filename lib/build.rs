6a128104ea989b04b72bcd2c3343123e582e96bf
Elon Aseneka <elonsantos63@gmail.com>
2025-05-07 19:40:19 +0300

    rectify longstanding gRPC OUT_DIR issue

--- lib/build.rs
+++ lib/build.rs
@@ -1,10 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // tonic_build::compile_protos("proto/acl/acl.proto")?;
-    // tonic_build::compile_protos("proto/email/email.proto")?;
-    // tonic_build::compile_protos("proto/files/files.proto")?;
-    // tonic_build::compile_protos("proto/products/products.proto")?;
-    // tonic_build::compile_protos("proto/orders/orders.proto")?;
-    // tonic_build::compile_protos("proto/payments/payments.proto")?;
     let out_dir = "src/integration/grpc/out";
 
     tonic_build::configure()
